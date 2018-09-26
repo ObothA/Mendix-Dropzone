8038d1d5bacacb78fd7e18e2e80c9235724af809
ObothA <khyper234@gmail.com>
2018-09-26 00:43:10 -0700

    optimise error messages for different scenarios

--- src/components/DropZone.ts
+++ src/components/DropZone.ts
@@ -18,20 +18,26 @@ interface DropZoneState {
     dropzoneObject: Dropzone | null;
     contextObject: mendix.lib.MxObject;
     maxFileSizeError: string;
+    fileTypeError: string;
+    generalError: string;
 }
 
 class DropZone extends Component<DropZoneProps, DropZoneState> {
     state: DropZoneState = {
         dropzoneObject: null,
         contextObject: this.props.mxObject,
-        maxFileSizeError: ""
+        maxFileSizeError: "",
+        fileTypeError: "",
+        generalError: ""
     };
 
     render() {
         return createElement("div", { className: "dropzoneContainer" },
             createElement("input", { type: "button", value: "upload file(s)", className: "uploadButton", onClick: this.handleUploud }),
             createElement("form", { className: "dropzone", id: "dropzoneArea" }),
-            createElement(Alert, { className: "widget-dropdown-type-ahead-alert" }, this.state.maxFileSizeError)
+            createElement(Alert, { className: "widget-dropdown-type-ahead-alert" }, this.state.maxFileSizeError),
+            createElement(Alert, { className: "widget-dropdown-type-ahead-alert" }, this.state.fileTypeError),
+            createElement(Alert, { className: "widget-dropdown-type-ahead-alert" }, this.state.generalError)
         );
     }
 
@@ -55,7 +61,7 @@ class DropZone extends Component<DropZoneProps, DropZoneState> {
             dictDefaultMessage: this.props.message,
             uploadMultiple: true,
             autoProcessQueue: false,
-            acceptedFiles: "",
+            acceptedFiles: ".pdf",
             addRemoveLinks: true
         });
 
@@ -72,57 +78,67 @@ class DropZone extends Component<DropZoneProps, DropZoneState> {
         }
 
         if (this.state.dropzoneObject) {
-        this.state.dropzoneObject.files.map((file) => {
-            mx.data.create({
-                entity: this.props.fileEntity,
-                callback: (obj) => {
-                    if (obj.isObjectReference(reference)) {
-                        obj.set(reference, this.state.contextObject.getGuid());
-                        // obj2 = obj;
+            this.state.dropzoneObject.files.map((file) => {
+                mx.data.create({
+                    entity: this.props.fileEntity,
+                    callback: (obj) => {
+                        if (obj.isObjectReference(reference)) {
+                            obj.set(reference, this.state.contextObject.getGuid());
+                            // obj2 = obj;
+                        }
+                        if (this.state.dropzoneObject) {
+                            mx.data.saveDocument(obj.getGuid(), file.name, {}, file,
+                                () => {
+                                    if (this.state.dropzoneObject) {
+                                        // state.dropzoneObject.processQueue();
+                                        this.state.dropzoneObject.removeAllFiles();
+                                    }
+                                },
+                                // tslint:disable-next-line:no-console
+                                (e) => { console.error(e); });
+                        }
+                    },
+                    error(e) {
+                        // tslint:disable-next-line:no-console
+                        console.error("Could not commit object:", e);
                     }
-                    if (this.state.dropzoneObject) {
-                        mx.data.saveDocument(obj.getGuid(), file.name, {}, file,
-                            () => {
-                                if (this.state.dropzoneObject) {
-                                    // state.dropzoneObject.processQueue();
-                                    this.state.dropzoneObject.removeAllFiles();
-                                }
-                            },
-                            // tslint:disable-next-line:no-console
-                            (e) => { console.error(e); });
-                    }
-                },
-                error(e) {
-                    // tslint:disable-next-line:no-console
-                    console.error("Could not commit object:", e);
-                }
+                });
             });
-        });
-    }
+        }
 
     }
 
     private handleErrors = (file: Dropzone.DropzoneFile, message: string) => {
-      if (message.toLowerCase().includes("file is too big")) {
-        const displayMessage = `${file.name} wont be uploaded, ${message}`;
-        this.setState({
-            maxFileSizeError: displayMessage
-            // rejectedFile: file.name
-         });
+        if (message.toLowerCase().includes("file is too big")) {
+            const displayMessage = `${file.name} wont be uploaded, ${message}`;
+            this.setState({
+                maxFileSizeError: displayMessage
+                // rejectedFile: file.name
+            });
 
-        if (this.state.dropzoneObject) {
-        this.state.dropzoneObject.removeFile(file);
+            if (this.state.dropzoneObject) {
+                this.state.dropzoneObject.removeFile(file);
+            }
         }
-    } else {
-      const displayMessage = `${file.name} wont be uploaded, ${message}`;
-      if (this.state.dropzoneObject) {
-        this.state.dropzoneObject.removeFile(file);
-        }
-      this.setState({
-        maxFileSizeError: displayMessage
-      });
-    }
 
+        if (message.toLowerCase().includes("You can't upload files of this type")) {
+            if (this.state.dropzoneObject) {
+                this.state.dropzoneObject.removeFile(file);
+            }
+            const displayMessage = `${file.name} wont be uploaded, ${message}`;
+            this.setState({
+                fileTypeError: displayMessage
+            });
+        } else {
+            const displayMessage = `${file.name} wont be uploaded, ${message}`;
+            if (this.state.dropzoneObject) {
+                this.state.dropzoneObject.removeFile(file);
+            }
+
+            this.setState({
+                generalError: displayMessage
+            });
+        }
     }
 }
 
